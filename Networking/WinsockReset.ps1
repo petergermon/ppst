f4ede996c643f5b06aa645de67024b018245e277
Peter Germon <petergermon@users.noreply.github.com>
2020-07-25 22:54:29 +1000

    Update WinsockReset.ps1

--- Networking/WinsockReset.ps1
+++ Networking/WinsockReset.ps1
@@ -7,7 +7,7 @@ $PSScriptRoot
 $resetWinsock= $PSScriptRoot+"\Modules\resetWinsock.ps1"
 
 Write-Host "--------------------------------------------------------------------------------------"
-Write-Host " WinPWSH-Toolkit - Winsock Reset"
+Write-Host " PowerShell-Technician-Toolkit - Winsock Reset"
 Write-Host " Powershell Script developed by Peter Germon under the GNU General Public License v3.0"
 Write-Host "--------------------------------------------------------------------------------------"
 Write-Host " "

af5e2c7a634130ebb363dee18a518ef876692366
Peter Germon <petergermon@users.noreply.github.com>
2020-07-25 16:56:10 +1000

    Add files via upload

---
+++ FixWindowsApps.ps1
@@ -0,0 +1,22 @@
+﻿<#
+    WinPWSH-Toolkit - FixWindowsApps
+    Powershell Script developed by Peter Germon under the GNU General Public License v3.0
+#>
+
+$PSScriptRoot
+$winapps= $PSScriptRoot+"\Modules\winapps.ps1"
+
+Write-Host "--------------------------------------------------------------------------------------"
+Write-Host " WinPWSH-Toolkit - FixWindowsApps"
+Write-Host " Powershell Script developed by Peter Germon under the GNU General Public License v3.0"
+Write-Host "--------------------------------------------------------------------------------------"
+Write-Host " "
+Write-Host " "
+
+Write-Host "-------------------------------------------------------"
+Write-Host " Reinstalling Windows Apps"
+Write-Host "-------------------------------------------------------"
+&$winapps
+Write-Host " "
+Write-Host " "
+Restart-Computer

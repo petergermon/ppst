a1054078d0538789726b73caeb6da0c5b27067a1
Peter Germon <petergermon@users.noreply.github.com>
2020-07-25 22:55:41 +1000

    Update FixWindowsUpdate.ps1

--- System/FixWindowsUpdate.ps1
+++ System/FixWindowsUpdate.ps1
@@ -7,7 +7,7 @@ $PSScriptRoot
 $resetWinUp= $PSScriptRoot+"\Modules\resetWinUp.ps1"
 
 Write-Host "--------------------------------------------------------------------------------------"
-Write-Host " WinPWSH-Toolkit - Fix Windows Update"
+Write-Host " PowerShell-Technician-Toolkit - Fix Windows Update"
 Write-Host " Powershell Script developed by Peter Germon under the GNU General Public License v3.0"
 Write-Host "--------------------------------------------------------------------------------------"
 Write-Host " "

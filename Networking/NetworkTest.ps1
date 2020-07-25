c54dda4b0853d633ef33377805cfb9fb514b628f
Peter Germon <petergermon@users.noreply.github.com>
2020-07-25 22:53:58 +1000

    Update NetworkTest.ps1

--- Networking/NetworkTest.ps1
+++ Networking/NetworkTest.ps1
@@ -10,7 +10,7 @@ $gateway= $PSScriptRoot+"\Modules\gateway.ps1"
 $ping= $PSScriptRoot+"\Modules\ping.ps1"
 
 Write-Host "--------------------------------------------------------------------------------------"
-Write-Host " WinPWSH-Toolkit - Network Test"
+Write-Host " PowerShell-Technician-Toolkit - Network Test"
 Write-Host " Powershell Script developed by Peter Germon under the GNU General Public License v3.0"
 Write-Host "--------------------------------------------------------------------------------------"
 Write-Host " "

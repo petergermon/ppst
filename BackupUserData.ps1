93cdf3458613c71e4dff25caf57afee75df8e50e
Peter Germon <petergermon@users.noreply.github.com>
2020-07-25 23:58:52 +1000

    Add files via upload

---
+++ BackupUserData.ps1
@@ -0,0 +1,21 @@
+﻿<#
+    PowerShell-Technician-Toolkit - Backup User Data
+    Powershell Script developed by Peter Germon under the GNU General Public License v3.0
+#>
+
+$PSScriptRoot
+$backup= $PSScriptRoot+"\Modules\backup.ps1"
+
+Write-Host "--------------------------------------------------------------------------------------"
+Write-Host " PowerShell-Technician-Toolkit - Backup User Data"
+Write-Host " Powershell Script developed by Peter Germon under the GNU General Public License v3.0"
+Write-Host "--------------------------------------------------------------------------------------"
+Write-Host " "
+Write-Host " "
+
+Write-Host "-------------------------------------------------------"
+Write-Host " Backup User Data"
+Write-Host "-------------------------------------------------------"
+&$backup
+Write-Host " "
+Write-Host " "
---
+++ backup.ps1
@@ -0,0 +1,2 @@
+﻿$BackupLocation = Read-Host -Prompt 'Enter Backup Location (e.g. F:\Backup): '
+Copy-Item -Path "C:\Users" -Destination "$BackupLocation" -Recurse

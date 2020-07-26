15573e548eae6e8f26f21fdffbe0d15bb733430c
Peter Germon <petergermon@users.noreply.github.com>
2020-07-26 11:10:23 +1000

    Add files via upload

---
+++ 100disk.ps1
@@ -0,0 +1,6 @@
+﻿Set-Service -Name "BITS" -StartupType Disabled
+Stop-Service -Force -Name "BITS"
+Set-Service -Name "SysMain" -StartupType Disabled
+Stop-Service -Force -Name "SysMain"
+Set-Service -Name "DiagTrack" -StartupType Disabled
+Stop-Service -Force -Name "DiagTrack"
